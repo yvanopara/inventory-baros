6c51f1946c4c30b9fca848db02fddff32f6bcd7a
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2026-03-04 22:30:09 +0100

    YES

--- backend/config/OauthKeys.js
+++ backend/config/OauthKeys.js
@@ -1,7 +1,7 @@
 //addmin Keys
-export const adminEmail = 'jeanbarora@gmail.com';
+export const adminEmail = 'yvanopara4000@gmail.com';
 export const adminPassword = '00000000';
 
 //UserKeys
-export const userEmail = 'jeanbarora@gmail.com';
+export const userEmail = 'yvanlandry4000@gmail.com';
 export const userPassword = '00000000';
